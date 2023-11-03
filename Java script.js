48693f33b603e55fedfc7213182f5948f30dae0c
Rogigermano <147650882+Rogigermano@users.noreply.github.com>
2023-11-03 19:39:30 -0300

    Add files via upload

---
+++ Java script.js
@@ -0,0 +1,8 @@
+document.addEventListener("click", function(event) {
+    if (event.target.classList.contains("buy-button")) {
+        // Você pode adicionar a lógica de compra aqui
+        alert("Carro comprado!");
+    }
+});
+
+    }
